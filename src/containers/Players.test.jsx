121c0f40fe2cac1bc5029f91f921f7dc085bb25f
Joseph Davis <joethebartenderpdx@gmail.com>
2021-07-27 15:24:11 -0700

    test setup

---
+++ src/containers/Players.test.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import PlayersMain from './PlayersMain';
+
+describe('PlayersMain component', () => {
+  it('renders PlayersMain', async () => {
+    render(<PlayersMain />);
+
+    screen.getByText('Almost...');
+
+  });
+});
