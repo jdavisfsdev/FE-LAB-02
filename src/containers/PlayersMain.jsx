4d3420e05df6e0e358becca62cf2b390bf83984c
Joseph Davis <joethebartenderpdx@gmail.com>
2021-07-27 15:44:01 -0700

    Loading on screen, test passes

--- src/containers/PlayersMain.jsx
+++ src/containers/PlayersMain.jsx
@@ -4,9 +4,12 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 export default class RickMortyMain extends Component {
     state = {
-      players: []
+      players: [],
+      loading: true
     };
     render() {
+      const { players, loading } = this.state;
+      if(loading) return <h1>Almost...</h1>;
       return (
         <div>
                 
