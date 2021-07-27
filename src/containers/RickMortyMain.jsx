a85eb2f21ad500c5b3c70c383b3e5f76818fbe4c
Joseph Davis <joethebartenderpdx@gmail.com>
2021-07-26 20:06:08 -0700

    folders created

---
+++ src/containers/RickMortyMain.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+export default class RickMortyMain extends Component {
+    state = {
+      name: '',
+      image: ''
+    };
+    render() {
+      return (
+        <div>
+                
+        </div>
+      );
+    }
+}
