c56224246b80873adb6aadde16539a5a2211cd1f
Joseph Davis <joethebartenderpdx@gmail.com>
2021-07-27 18:03:25 -0700

    list of players diaplays

--- src/containers/PlayersMain.jsx
+++ src/containers/PlayersMain.jsx
@@ -1,19 +1,31 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+// import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import PlayerList from '../components/PlayerList';
 
+const getPlayers = async () => {
+  const res = await fetch('https://www.balldontlie.io/api/v1/players');
+  const json = await res.json();
+  return json.data;
+};
 
-export default class RickMortyMain extends Component {
+export default class PlayersMain extends Component {
     state = {
       players: [],
       loading: true
     };
+    
+    async componentDidMount() {
+      const players = await getPlayers();
+      this.setState({ players, loading:false });
+    }
+
     render() {
       const { players, loading } = this.state;
       if(loading) return <h1>Almost...</h1>;
-      return (
-        <div>
-                
-        </div>
-      );
+      return <PlayerList players={players} />;
+
+          
+
+
     }
 }
